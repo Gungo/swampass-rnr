0ad34b7ecc5c9bd2e4f6b1fe7317eef28a834208
gungo <gungo06@ufl.edu>
2018-12-09 02:42:08 -0500

    Updated css.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :toilet
-  
+
+  validates :username, presence: true, length: {maximum: 36}
+  validates :body,     presence: true, length: {maximum: 128}
 end
--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,8 +1,9 @@
 class Toilet < ApplicationRecord
   has_many :comments, :dependent => :delete_all
 
-  validates  :name,        presence: true, length: {minimum: 5}
-  validates  :address,     presence: true
-  validates  :poop_rating, presence: true, length:{maximum: 10}
-  validates  :atmosphere,  presence: true, length:{maximum: 10}
+  validates  :name,        presence: true, length: { minimum: 5, maximum: 128 }
+  validates  :address,     presence: true, length: { maximum: 128}
+  validates  :location,    presence: true, length: { maximum: 128}
+  validates  :poop_rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates  :atmosphere,  presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
 end
